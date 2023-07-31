183b4bfeb6c657c296a558c43d8eff73d1d56d1b
kojo8888 <kojo88@gmx.net>
2023-07-31 19:06:54 +0200

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: "kojo88@gmx.net",
   link: "https://nobelium.vercel.app",
   description: "Erfahrungen eines dualen Studenten",
-  lang: "de-DE", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: "Etc/UCT", // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: "auto", // ['light', 'dark', 'auto'],
   font: "sans-serif", // ['sans-serif', 'serif']
